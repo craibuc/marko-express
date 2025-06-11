44168b682ec5e268e48f0f87a6b53b38cfbd8b62
Craig Buchanan <craig.buchanan@cogniza.com>
2025-06-11 18:03:48 +0000

    adding skin-color functionality

---
+++ src/routes/api/characters/skin_color/+handler.ts
@@ -0,0 +1,20 @@
+export const GET = (async (ctx) => {
+  console.log("GET /api/characters/skin_color");
+  
+  const data = await fetch('https://api.sampleapis.com/avatar/characters')
+    .then((resp) => resp.json())
+    .then((data) => {
+        const set = new Set(
+        data
+            .map(item => item.physicalDescription?.skinColor)
+            .filter(Boolean)
+        );
+
+        return Array.from(set).sort();
+    });
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+
+}) satisfies MarkoRun.Handler;
